c18a9aacd4a39556673e2d41d72c125de45f26d7
Matias Dominguez <mdrdev83@gmail.com>
2026-03-14 21:54:38 -0300

    primera version

---
+++ android/app/src/main/java/cl/solarsol/app/MainActivity.java
@@ -0,0 +1,5 @@
+package cl.solarsol.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,30 @@
+﻿import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cl.solarsol.app',
+  appName: 'Solarsol',
+  webDir: 'www',
+  server: {
+    url: 'https://www.solarsol.cl/app.html',
+    cleartext: false,
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: '#12141A',
+      showSpinner: false,
+    },
+    PushNotifications: {
+      presentationOptions: ['badge', 'sound', 'alert'],
+    },
+  },
+  ios: {
+    contentInset: 'always',
+    allowsLinkPreview: false,
+  },
+  android: {
+    allowMixedContent: false,
+  },
+};
+
+export default config;
